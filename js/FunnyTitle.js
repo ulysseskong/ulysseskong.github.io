bfb9dc230d2ce278890e1421e958ea91980a974c
ulysseskong <k17087799942@gmail.com>
2021-12-11 20:22:36 +0800

    Site updated: 2021-12-11 20:22:29

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
-        document.title = '你好哇 小路！';
+        document.title = '再看我一眼吗';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
-        document.title = '你好啊 孔宇航！' + OriginTitle;
+        document.title = '你好哇 小路' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
